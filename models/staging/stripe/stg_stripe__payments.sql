4280d6b8be1d67a20241e1148342b8c8cd0f40fb
SCendejas <scendejasm@gmail.com>
2026-09-07 11:42:06 -0700

    Configured sources jaffle shop customers, orders, and stripe payments

---
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -0,0 +1,12 @@
+
+select
+    ID as payment_id,
+    ORDERID as order_id,
+    PAYMENTMETHOD as payment_method,
+    STATUS as status,
+
+    -- amount is stored in cents, convert to dollars
+    AMOUNT / 100 as amount,
+    CREATED as created_at
+
+from {{ source('stripe', 'payments') }}
