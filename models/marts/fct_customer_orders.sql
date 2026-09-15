c89b08e2ae68845182de2242ff5a7b9e7e391d8d
SCendejas <scendejasm@gmail.com>
2026-09-15 09:51:13 -0700

    added fct_customer_orders to be developed in parallel with customer_orders_lagacy

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -1,35 +1,45 @@
-select
+-- Import CTEs
+with raw_customers as (
+    select * from {{ source('jaffle_shop','customers') }}
+),
+raw_orders as (
+    select * from {{ source('jaffle_shop','orders') }}
+),
+raw_payments as (
+    select * from {{ source('stripe','payments') }}
+),
 
-    orders.id as order_id,
-    orders.user_id as customer_id,
-    last_name as surname,
-    first_name as givenname,
-    first_order_date, 
-    order_count,
-    total_lifetime_value,
-    round(amount/100.0,2) as order_value_dollars,
-    orders.status as orders_status,
-    payments.status as payment_status
-
-from {{ source('jaffle_shop','orders') }}
-
-join (
+-- Logical CTEs
+customers as (
 
     select
-        first_name || ' ' || last_name as name,
+        id as customer_id,
+        first_name as givenname,
+        last_name as surname,
+        first_name || ' ' || last_name as full_name
+    from raw_customers 
+
+),
+
+a as (
+
+    select
+        row_number() over (
+            partition by user_id 
+            order by order_date,id
+        ) as user_order_seq,
         *
-    from {{ source('jaffle_shop','customers') }}
+    from raw_orders 
 
-) customers
-on orders.user_id = customers.id
+),
 
-join ( 
+customer_order_history as ( 
 
     select
-        b.id as customer_id,
-        b.name as full_name, 
-        b.last_name as surname,
-        b.first_name as givenname,
+        customers.customer_id,
+        customers.full_name, 
+        customers.surname,
+        customers.givenname,
         min(order_date) as first_order_date,
         min(
             case 
@@ -77,39 +87,51 @@ join (
             ) as avg_non_returned_order_value,
         array_agg(distinct a.id) as order_ids
 
-    from (
-        select
-            row_number() over (
-                partition by user_id 
-                order by order_date,id
-            ) as user_order_seq,
-            *
-        from {{ source('jaffle_shop','orders') }} 
+    from  a
 
-    ) a
+    join  customers
 
-    join (
+    on a.user_id = customers.customer_id
 
-        select
-            first_name || ' ' || last_name as name,
-            *
-        from {{ source('jaffle_shop','customers') }}
-
-    ) b
-
-    on a.user_id = b.id
-
-    left outer join {{ source('stripe','payments') }} c
+    left outer join raw_payments c
     on a.id = c.orderid
 
     where a.status not in ('pending') and c.status != 'fail'
 
-    group by b.id, b.name, b.last_name, b.first_name
+    group by customers.customer_id, customers.full_name, customers.surname, customers.givenname
 
-) customer_order_history
-on orders.user_id = customer_order_history.customer_id
+),
 
-left outer join {{ source('stripe','payments') }}
-on orders.id = payments.orderid 
+-- Final CTE
+final as (
 
-where payments.status != 'fail'
+    select
+
+        orders.id as order_id,
+        orders.user_id as customer_id,
+        customers.surname,
+        customers.givenname,
+        first_order_date, 
+        order_count,
+        total_lifetime_value,
+        round(amount/100.0,2) as order_value_dollars,
+        orders.status as orders_status,
+        payments.status as payment_status
+
+    from raw_orders as orders
+
+    join customers
+    on orders.user_id = customers.customer_id
+
+    join  customer_order_history
+    on orders.user_id = customer_order_history.customer_id
+
+    left outer join raw_payments payments 
+    on orders.id = payments.orderid 
+
+    where payments.status != 'fail'
+
+)
+
+-- Final Select 
+select * from final
